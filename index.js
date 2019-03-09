eb532739c40259871e7e113556f8646734231f23
120356aa <yurlovandrew@gmail.com>
2019-03-08 18:23:53 -0800

    get req for projects and actions

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ const actionsDB = require('./data/helpers/actionModel');
 
 server.use(express.json());
 
+// GET PROJECTS AND GET ACTIONS
+server.get('/api/projects', async (req, res) => {
+  try {
+    const projects = await projectDB.get();
+    res.status(200).json(projects);
+  } catch (err) {
+    res.status(500).json({error: 'Unable to get projects'});
+  }
+});
 
+server.get('/api/actions', async (req, res) => {
+  try {
+    const actions = await actionsDB.get();
+    res.status(200).json(actions);
+  } catch (err) {
+    res.status(500).json({error: 'Unable to get actions'});
+  }
+});
 
 server.listen(4000);
